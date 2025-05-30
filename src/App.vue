9210f9a099275558dcfacacc60907e1abf062741
Toufiq <mdtoufiqulalam64@gmail.com>
2025-05-30 20:27:24 +0600

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+
+<h1>{{ products }}</h1>
+</template>
+
+
+
+<script setup>
+import {ref} from "vue";
+
+const products = ref([])
+
+CallAPI();
+async function CallAPI(){
+  const response = await fetch("https://dummyjson.com/products");
+  const data = await response.json();
+  console.log(data);
+  products.value = data['products'];
+}
+
+</script>
---
+++ src/components/part1.vue
@@ -0,0 +1,24 @@
+<template>
+  
+
+<h1>{{ number }}</h1>
+
+<button @click="plus">ADD</button>
+<button @click="minus">Minus</button>
+
+</template>
+
+<script setup>
+import {ref} from "vue";
+let number =ref(900);
+const plus =()=>{
+number.value= number.value +1;
+console.log(number);
+}
+const minus =()=>{
+
+number.value = number.value -1;
+console.log(number);
+}
+
+</script>
